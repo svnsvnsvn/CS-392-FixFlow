8e3c8f841e894d64eac89325f2699edf1f4978fd
Adam M. Sexton <AdamMSexton@Live.com>
2026-03-15 16:59:18 -0400

    Rough/sketch version of user tool copied over and implemented.  Need to refine role names.

---
+++ fixflow.web/wwwroot/js/UserSearch.js
@@ -0,0 +1,37 @@
+﻿
+function InitalizeUserSearch(searchBoxId, resultsBoxId, url)
+{
+    const inputBox = document.getElementById(searchBoxId)
+    const resultsBox = document.getElementById(resultsBoxId)
+    
+    let debounceTimer;
+
+    //console.log(inputBox);
+    //console.log(resultsBox);
+
+    inputBox.addEventListener("input", function () {
+        const term = this.value;
+        if (term.length > 2) {
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(async () => {
+                const response = await fetch(url + "&term=" + encodeURIComponent(term));
+                const data = await response.json();
+                //console.log(data);
+
+                updateSearchResults(resultsBox, data);
+            }, 300);
+        }
+    });
+}
+
+function updateSearchResults(_resultsBox, _results) {
+    _resultsBox.replaceChildren();
+    _results.forEach(user => {
+        const option = document.createElement("option")
+        option.value = user.userId;
+        option.title = user.role;
+        option.textContent = user.lName + ", " + user.fName;
+
+        _resultsBox.appendChild(option);
+    });
+}
---
+++ fixflow.web/wwwroot/js/UserSettings.js
@@ -0,0 +1,36 @@
+﻿function InitalizeUserSettingsFetcher(selectorListId, url)
+{
+    const selectorList = document.getElementById(selectorListId)
+
+    //console.log(selectorList)
+
+    selectorList.addEventListener("change", async function () {
+        const selectedUser = this.value;
+
+        //console.log("Change Hit");
+        //console.log(selectedUser);
+        const response = await fetch(url + "&selectedUser=" + encodeURIComponent(selectedUser));
+        const data = await response.json();
+        console.log(data);
+
+        updateUserSettings(data);
+    });
+
+
+}
+
+function updateUserSettings(_data)
+{
+    if (_data.role === "Pending")
+    {
+        document.querySelectorAll('input[name="SelectedUserRole"]').forEach(r => r.checked = false);
+    }
+    else
+    {
+        document.querySelector(`input[name="SelectedUserRole"][value="${_data.role}"]`).checked = true;
+    }
+    
+    document.getElementById("LoginPassAllowed").checked = _data.loginPassAllowed;
+    document.getElementById("OIDCAllowed").checked = _data.oidcAllowed;
+    document.getElementById("ResetPassOnLogin").checked = _data.resetPassOnLogin;
+}
